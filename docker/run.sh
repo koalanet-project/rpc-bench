a29e9ed0621e484df42b1e8ab1834d31c9cad2e8
Jingrong Chen <crazyboycjr@gmail.com>
2020-06-17 03:05:36 +0800

    pre-install perf in Dockerfile, give SYS_ADMIN capability to run perf in container

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VOLUME="--volume /mnt:/mnt "
-docker run -it --name rpc-bench-box --net=host --hostname rpc-bench-host $VOLUME --cap-add SYS_PTRACE rpc-bench-box zsh
+docker run -it --name rpc-bench-box --net=host --hostname rpc-bench-host $VOLUME --cap-add SYS_PTRACE --cap-add SYS_ADMIN rpc-bench-box zsh
 
 # you have to copy the source code into the container manually
