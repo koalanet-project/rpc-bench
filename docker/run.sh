f7586d4da471a4ec4f0a5650a578ada861041489
Jingrong Chen <crazyboycjr@gmail.com>
2020-07-10 17:49:49 +0800

    default to use minimized ubuntu container

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VOLUME="--volume /mnt:/mnt "
-docker run -it --name rpc-bench-box --net=host --hostname rpc-bench-host $VOLUME --cap-add SYS_PTRACE --cap-add SYS_ADMIN rpc-bench-box zsh
+# docker run -it --name rpc-bench-box --net=host --hostname rpc-bench-host $VOLUME --cap-add SYS_PTRACE --cap-add SYS_ADMIN rpc-bench-box zsh
+docker run -it --name rpc-bench-box --net=host --hostname rpc-bench-host $VOLUME --cap-add SYS_PTRACE --cap-add SYS_ADMIN rpc-bench-box bash
 
 # you have to copy the source code into the container manually
