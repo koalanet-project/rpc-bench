ec9f642d604135e012ea51a1d0a231e2fe9f9510
Jingrong Chen <crazyboycjr@gmail.com>
2020-07-10 17:59:57 +0800

    Optimize Dockerfile and Makefile (#11)
    * minor improvement on Makefile, grpc, and brpc dependencies
    
    * default to use minimized ubuntu container

--- make/deps.mk
+++ make/deps.mk
@@ -25,7 +25,7 @@ $(GRPC):
 	$(eval DIR=grpc-v1.29.1)
 	rm -rf $(FILE) $(DIR)
 	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
-	cd $(DIR) && mkdir -p cmake/build && cd cmake/build && cmake -Dprotobuf_WITH_ZLIB=ON -DgRPC_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF -DCMAKE_INSTALL_PREFIX=$(DEPS_PATH) ../.. && $(MAKE) && $(MAKE) install
+	cd $(DIR) && mkdir -p cmake/build && cd cmake/build && cmake -Dprotobuf_WITH_ZLIB=ON -DgRPC_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF -DCMAKE_INSTALL_PREFIX=$(DEPS_PATH) ../.. && $(MAKE) && $(MAKE) install
 	rm -rf $(FILE) $(DIR)
 
 GFLAGS := $(DEPS_PATH)/include/gflags/gflags.h
@@ -53,7 +53,7 @@ $(BRPC): $(GFLAGS) $(LEVELDB) $(GRPC)
 	$(eval DIR=incubator-brpc-0.9.7)
 	rm -rf $(FILE) $(DIR)
 	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
-	cd $(DIR) && sed -i '173s#\(.*\)-lssl -lcrypto\(.*\)#\1/usr/lib/libssl.so /usr/lib/libcrypto.so\2#g' config_brpc.sh && PATH=$(DEPS_PATH)/bin:$(PATH) sh config_brpc.sh --headers="$(DEPS_PATH)/include /usr/include" --libs="$(DEPS_PATH)/lib /usr/lib64" && $(MAKE) && cp -r output/* $(DEPS_PATH)
+	cd $(DIR) && sed -i "173s#\(.*\)-lssl -lcrypto\(.*\)#\1`pkg-config --variable=libdir libssl`/libssl.so `pkg-config --variable=libdir libcrypto`/libcrypto.so\2#g" config_brpc.sh && PATH=$(DEPS_PATH)/bin:$(PATH) sh config_brpc.sh --headers="$(DEPS_PATH)/include /usr/include" --libs="$(DEPS_PATH)/lib /usr/lib64" && $(MAKE) && cp -r output/* $(DEPS_PATH)
 	rm -rf $(FILE) $(DIR)
 
 ZEROMQ := $(DEPS_PATH)/include/zmq.h
