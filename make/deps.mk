1507a15c8a7160ad285acf0b75e470796886e073
lsh <lshzy137@gmail.com>
2022-03-09 17:57:07 -0500

    updated grpc and brpc version

--- make/deps.mk
+++ make/deps.mk
@@ -21,12 +21,19 @@ $(GOOGLE_BENCHMARK):
 
 GRPC := $(DEPS_PATH)/include/grpcpp/grpcpp.h
 $(GRPC):
-	$(eval FILE=grpc-v1.29.1.tar.gz)
-	$(eval DIR=grpc-v1.29.1)
-	rm -rf $(FILE) $(DIR)
-	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
-	cd $(DIR) && mkdir -p cmake/build && cd cmake/build && cmake -Dprotobuf_WITH_ZLIB=ON -DgRPC_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF -DCMAKE_INSTALL_PREFIX=$(DEPS_PATH) ../.. && $(MAKE) && $(MAKE) install
-	rm -rf $(FILE) $(DIR)
+	$(eval DIR=grpc)
+	git clone --recurse-submodules -b v1.43.0 https://github.com/grpc/grpc
+	cd $(DIR) && mkdir -p cmake/build && cd cmake/build && cmake -DCMAKE_BUILD_TYPE=Release -Dprotobuf_WITH_ZLIB=ON -DgRPC_INSTALL=ON -DBUILD_SHARED_LIBS=ON -DgRPC_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$(DEPS_PATH) ../.. && $(MAKE) && $(MAKE) install
+	rm -rf $(DIR)
+
+# GRPC := $(DEPS_PATH)/include/grpcpp/grpcpp.h
+# $(GRPC):
+# 	$(eval FILE=grpc-v1.29.1.tar.gz)
+# 	$(eval DIR=grpc-v1.29.1)
+# 	rm -rf $(FILE) $(DIR)
+# 	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+# 	cd $(DIR) && mkdir -p cmake/build && cd cmake/build && cmake -Dprotobuf_WITH_ZLIB=ON -DgRPC_INSTALL=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF -DCMAKE_INSTALL_PREFIX=$(DEPS_PATH) ../.. && $(MAKE) && $(MAKE) install
+# 	rm -rf $(FILE) $(DIR)
 
 GFLAGS := $(DEPS_PATH)/include/gflags/gflags.h
 $(GFLAGS):
@@ -49,12 +56,19 @@ $(LEVELDB):
 
 BRPC := $(DEPS_PATH)/include/brpc/channel.h
 $(BRPC): $(GFLAGS) $(LEVELDB) $(GRPC)
-	$(eval FILE=incubator-brpc-0.9.7.tar.gz)
-	$(eval DIR=incubator-brpc-0.9.7)
-	rm -rf $(FILE) $(DIR)
-	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
-	cd $(DIR) && sed -i "173s#\(.*\)-lssl -lcrypto\(.*\)#\1`pkg-config --variable=libdir libssl`/libssl.so `pkg-config --variable=libdir libcrypto`/libcrypto.so\2#g" config_brpc.sh && PATH=$(DEPS_PATH)/bin:$(PATH) sh config_brpc.sh --headers="$(DEPS_PATH)/include /usr/include" --libs="$(DEPS_PATH)/lib /usr/lib64" && $(MAKE) && cp -r output/* $(DEPS_PATH)
-	rm -rf $(FILE) $(DIR)
+	$(eval DIR=incubator-brpc)
+	git clone https://github.com/apache/incubator-brpc.git
+	cd $(DIR) && sed -i "173s#\(.*\)-lssl -lcrypto\(.*\)#\1`pkg-config --variable=libdir libssl`/libssl.so `pkg-config --variable=libdir libcrypto`/libcrypto.so\2#g" config_brpc.sh && PATH=$(DEPS_PATH)/bin:$(PATH) sh config_brpc.sh --headers="$(DEPS_PATH)/include /usr/include" --libs="$(DEPS_PATH)/lib /usr/lib64" --nodebugsymbols && $(MAKE) && cp -r output/* $(DEPS_PATH)
+	rm -rf $(DIR)
+
+# BRPC := $(DEPS_PATH)/include/brpc/channel.h
+# $(BRPC): $(GFLAGS) $(LEVELDB) $(GRPC)
+# 	$(eval FILE=incubator-brpc-0.9.7.tar.gz)
+# 	$(eval DIR=incubator-brpc-0.9.7)
+# 	rm -rf $(FILE) $(DIR)
+# 	$(WGET) $(URL)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+# 	cd $(DIR) && sed -i "173s#\(.*\)-lssl -lcrypto\(.*\)#\1`pkg-config --variable=libdir libssl`/libssl.so `pkg-config --variable=libdir libcrypto`/libcrypto.so\2#g" config_brpc.sh && PATH=$(DEPS_PATH)/bin:$(PATH) sh config_brpc.sh --headers="$(DEPS_PATH)/include /usr/include" --libs="$(DEPS_PATH)/lib /usr/lib64" && $(MAKE) && cp -r output/* $(DEPS_PATH)
+# 	rm -rf $(FILE) $(DIR)
 
 ZEROMQ := $(DEPS_PATH)/include/zmq.h
 $(ZEROMQ):
