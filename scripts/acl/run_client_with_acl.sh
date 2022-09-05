342265d762a60f15b51e44227193b27955a22c1b
Jingrong Chen <crazyboycjr@gmail.com>
2022-09-05 17:32:17 -0400

    add some scripts

---
+++ scripts/acl/run_client_with_acl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+WORKDIR=`dirname $(realpath $0)`
+cd $WORKDIR/../../
+
+# Run this on danyang-05 as client
+numactl -N 0 -m 0  ./build/rpc-bench -a hotel_reservation -r grpc -d 32 --concurrency 32 -p 10001 -t 10 127.0.0.1
