9e54a3ab4d0590f18e20de53b09520d5ac10ca10
lsh <lshzy137@gmail.com>
2022-09-06 00:24:32 -0400

    updated throughput scaling with threads

--- scripts/bench_util.py
+++ scripts/bench_util.py
@@ -6,7 +6,7 @@ import time
 
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument('server', metavar='server',
-                         type=str, nargs='?', default='192.168.211.130')
+                         type=str, nargs='?', default='rdma0.danyang-06')
 args_parser.add_argument(
     '--opt', type=str, nargs='?', default='', help="Options of rpc-bench")
 
@@ -15,6 +15,8 @@ opt_parser.add_argument('-a', type=str, nargs='?')
 opt_parser.add_argument('-p', type=int, nargs='?', default=18000)
 opt_parser.add_argument('-t', type=int, nargs='?', default=10)
 opt_parser.add_argument('-c', type=int, nargs='?', default=32)
+opt_parser.add_argument('-T', type=int, nargs='?', default=1)
+opt_parser.add_argument('-d', type=int, nargs='?', default=32)
 
 
 def ssh_cmd(server, cmd):
@@ -40,8 +42,8 @@ def run_server(args, opt):
         envoy_cmd = "nohup envoy -c envoy/envoy.yaml >scripts/%s/envoy_server.log 2>&1 &" % opt.a
     server_script = ''' 
     cd ~/nfs/Developing/rpc-bench; %s;
-    GLOG_minloglevel=2 nohup numactl -N 0 -m 0 ./build/rpc-bench -a %s -r grpc -d %d >/dev/null 2>&1 & 
-    ''' % (envoy_cmd, opt.a, opt.d)
+    GLOG_minloglevel=2 nohup ./build/rpc-bench -a %s -r grpc -d %d -T %d >/dev/null 2>&1 & 
+    ''' % (envoy_cmd, opt.a, opt.d, opt.T)
     print('server envoy:', envoy_cmd)
     print('server:', server_script)
     ssh_cmd(args.server, server_script)
@@ -56,8 +58,8 @@ def run_client(args, opt):
             "nohup envoy -c envoy/envoy.yaml >scripts/%s/envoy_client.log 2>&1 &" % opt.a)
         env = dict(env, http_proxy="http://127.0.0.1:10000/")
     client_script = '''
-    numactl -N 0 -m 0  ./build/rpc-bench -a %s -r grpc -d %d --concurrency %d -p %d -t %d %s
-    ''' % (opt.a, opt.d, opt.c, opt.p, opt.t, args.server)
+    ./build/rpc-bench -a %s -r grpc -d %d -C %d -T %d -p %d -t %d %s
+    ''' % (opt.a, opt.d, opt.c, opt.T, opt.p, opt.t, args.server)
     print('client:', client_script)
     with subprocess.Popen(client_script.split(), env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
         out, err = proc.communicate()
--- scripts/throughput/bench_tput.py
+++ scripts/throughput/bench_tput.py
@@ -1,52 +1,67 @@
 import os
 import sys
 import copy
+import re
 import csv
+import numpy as np
 
 sys.path.append("..")
 import bench_util as util
 
 util.args_parser.add_argument(
-    '--range', type=str, nargs='?', default='1,12', help="l,r means [1<<l, 1<<r] Byte")
+    '--range', type=str, nargs='?', default='0,4', help="l,r means [1<<l, 1<<r] Byte")
 util.args_parser.add_argument(
-    '--step', type=int, nargs='?', default=2, help="Range step")
+    '--step', type=int, nargs='?', default=1, help="Range step")
 args = util.args_parser.parse_args()
 args.range = range(*map(int, args.range.split(',')), args.step)
 
 opt = util.opt_parser.parse_args(("%s -a throughput" % args.opt).split())
 os.chdir("../../")
 
+pattern = re.compile(r"\[meter\s*?([0-9]*?)\]")
+pattern_rps = re.compile(r"([0-9]*?\.?[0-9]*?)\sops/s")
+
+
+def parse_result(out):
+    out = out[2:]
+    cnt = {}
+    ops = {}
+    for line in out:
+        tid = re.findall(pattern, line)[0]
+        rps = float(re.findall(pattern_rps, line)[0])
+        if tid not in cnt:
+            cnt[tid] = 0
+            ops[tid] = []
+        cnt[tid] += 1
+        if cnt[tid] >= 2:
+            ops[tid].append(rps)
+    n = 1000000
+    for tid in ops:
+        n = min(n, len(ops[tid]))
+    res = []
+    for i in range(0, n):
+        res.append(0)
+        for tid in ops:
+            res[i] += ops[tid][i]
+    return res
+
+
 res = []
 for k in args.range:
+    opt.T = 1 << k
     args.envoy = False
-    opt.d = 1 << k
-    print("Running size %d" % opt.d, file=sys.stderr)
+    print("Running %d threads" % opt.T, file=sys.stderr)
     util.killall(args)
     util.run_server(args, opt)
     out = util.run_client(args, opt)
 
-    items = out[-1].split()
-    res.append(float(items[2]))
-
-res_envoy = []
-for k in args.range:
-    args.envoy = True
-    opt.d = 1 << k
-    print("Running size %d with envoy" % opt.d, file=sys.stderr)
-    util.killall(args)
-    util.run_server(args, opt)
-    opt_client = copy.deepcopy(opt)
-    opt_client.p = 10001  # envoy port
-    out = util.run_client(args, opt_client)
-
-    items = out[-1].split()
-    res_envoy.append(float(items[2]))
+    ops = parse_result(out)
+    for x in ops:
+        res.append((opt.T, x))
 
 util.killall(args)
 
 writer = csv.writer(sys.stdout, lineterminator='\n')
-writer.writerow(['Message Size (Byte)', 'Throughput (rps)', 'Solution'])
-for k, y in zip(args.range, res):
-    writer.writerow([1 << k, y, "gRPC"])
-for k, y in zip(args.range, res_envoy):
-    writer.writerow([1 << k, y, "gRPC (envoy)"])
+writer.writerow(['# Client Threads', 'RPC Rate (rps)', 'Solution'])
+for k, y in res:
+    writer.writerow([k, y / 1e6, "gRPC"])
