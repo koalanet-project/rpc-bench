e114e56d26b02b3e3e116fa9f3e7c32fb7c70cb6
lbr <lichangxinlivingshade@gmail.com>
2023-06-28 14:10:54 -0400

    add for loop based cl

---
+++ for-acl/src/lib.rs
@@ -0,0 +1,119 @@
+use proxy_wasm::traits::{Context, HttpContext};
+use proxy_wasm::types::{Action, LogLevel};
+
+use prost::Message;
+use std::vec::Vec;
+// pub mod lat_tput_app {
+//     include!(concat!(env!("OUT_DIR"), "/rpc_bench.lat_tput_app.rs"));
+// }
+
+pub mod reservation {
+    include!(concat!(env!("OUT_DIR"), "/reservation.rs"));
+}
+
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_http_context(|_, _| -> Box<dyn HttpContext> {
+        Box::new(AccessControl::default())
+    });
+}
+
+struct AccessControlRecord {
+    #[allow(unused)]
+    name: String,
+    #[allow(unused)]
+    permission: String,
+}
+
+struct AccessControl {
+    #[allow(unused)]
+    access_control_list: Vec<AccessControlRecord>,
+}
+
+impl AccessControl {
+    pub fn default() -> Self {
+        let mut ret = AccessControl {
+            access_control_list: Vec::new(),
+        };
+        ret.access_control_list.push(AccessControlRecord {
+            name: "Apple".to_string(),
+            permission: "N".to_string(),
+        });
+        ret.access_control_list.push(AccessControlRecord {
+            name: "Banana".to_string(),
+            permission: "Y".to_string(),
+        });
+        ret
+    }
+}
+
+impl Context for AccessControl {}
+
+impl HttpContext for AccessControl {
+    fn on_http_request_headers(&mut self, _num_of_headers: usize, end_of_stream: bool) -> Action {
+        // log::info!("on_http_request_headers, num_of_headers: {num_of_headers}, end_of_stream: {end_of_stream}");
+
+        // for (name, value) in &self.get_http_request_headers() {
+        //     log::info!("In WASM : #{} -> {}: {}", self.context_id, name, value);
+        // }
+
+        if !end_of_stream {
+            return Action::Continue;
+        }
+
+        // If there is a Content-Length header and we change the length of
+        // the body later, then clients will break. So remove it.
+        // We must do this here, because once we exit this function we
+        // can no longer modify the response headers.
+        self.set_http_response_header("content-length", None);
+        Action::Continue
+    }
+
+    fn on_http_request_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
+        // log::info!("on_http_request_body, body_size: {body_size}, end_of_stream: {end_of_stream}");
+        if !end_of_stream {
+            // Wait -- we'll be called again when the complete body is buffered
+            // at the host side.
+            // Returns Continue because there's other filters to process. Return Pause may cause
+            // problem in this case.
+            // if let Some(body) = self.get_http_request_body(0, body_size) {
+            //     log::info!("body: {:?}", body);
+            // }
+            // return Action::Continue;
+            return Action::Pause;
+        }
+
+        // Replace the message body if it contains the text "secret".
+        // Since we returned "Pause" previuously, this will return the whole body.
+        if let Some(body) = self.get_http_request_body(0, body_size) {
+            // log::info!("body: {:?}", body);
+            // Parse grpc payload, skip the first 5 bytes
+            match reservation::Request::decode(&body[5..]) {
+                Ok(req) => {
+                    // log::info!("req: {:?}", req);
+                    for rec in &self.access_control_list {
+                        if req.customer_name == rec.name {
+                            if rec.permission == "Y" {
+                                return Action::Continue;
+                            } else {
+                                self.send_http_response(
+                                    200,
+                                    vec![
+                                        ("grpc-status", "1"),
+                                        ("grpc-message", "Access forbidden.\n"),
+                                    ],
+                                    None,
+                                );
+                                return Action::Pause;
+                            }
+                        }
+                    }
+                }
+                Err(e) => log::warn!("decode error: {}", e),
+            }
+        }
+
+        Action::Continue
+    }
+}
